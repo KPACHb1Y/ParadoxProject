d3cec6d88dc8f8e1a0184038a51fe64361bffa96
KPACHb1Y <barb0s9109@mail.ru>
2020-01-22 13:21:46 +0300

    create new function

--- app/js/index.js
+++ app/js/index.js
@@ -1,46 +1,41 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const createRow = () => {
-//         const parent = document.getElementById('parent');
-//         const row = document.createElement('div');
-//         const p = document.createElement('p');
-//         const dayArray = ['Пн','Вт','Ср','Чт','Пт','Сб','Вс'];
-//         for (let i = 0; i < day) {
-//
-//         }
-//         dayArray.map((item, i) => {
-//             console.log(i);
-//             p.innerHTML = item;
-//         });
-//         row.className = "row";
-//         parent.appendChild(row);
-//         row.appendChild(p);
-//         return row;
-//     };
-//
-//     const createElement = parent => {
-//         const item = document.createElement('div');
-//         item.className = "item";
-//         parent.appendChild(item);
-//     };
-//
-//     for (let i = 0; i < 7; i++) {
-//         const row = createRow();
-//         for (let k = 0; k < 16; k++) {
-//             createElement(row);
-//         }
-//     }
-//
-//
-//
-// });
+document.addEventListener('DOMContentLoaded', () => {
+    for (var i = 0; i < 24; i++) {
+        var row = createRow();
+        for (var k = 0; k < 7; k++) {
+            createElement(row);
+        };
+    };
 
-const items = document.querySelectorAll('.item');
-items.forEach(links => {
-    links.addEventListener('click', e => {
-        if (!e.ctrlKey) {
-            (!e.target.classList.contains('active'))
-                ? e.target.classList.add('active')
-                : e.target.classList.remove('active');
-        }
+    function createRow() {
+        var parent = document.getElementById('parent');
+        var row = document.createElement('div');
+        row.className = "row";
+        parent.appendChild(row);
+        return row;
+    }
+
+    function createElement(parent) {
+        var item = document.createElement('div');
+        item.className = "item";
+        parent.appendChild(item);
+    }
+
+    const items = document.querySelectorAll('.item');
+    items.forEach(links => {
+        links.addEventListener('mousedown', e => {
+            e.target.classList.toggle('active');
+            mouseMove();
+        });
+
+        links.addEventListener('mouseup', e => {
+            e.target.classList.add('active');
+            !mouseMove();
+        });
     });
+
+    const mouseMove = () => {
+        document.addEventListener('mouseover', e => {
+            e.target.classList.toggle('active');
+        });
+    };
 });
